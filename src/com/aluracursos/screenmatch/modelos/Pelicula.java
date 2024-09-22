ca9dc80ee1e29c49cbf988d4b35dfbf31c584f22
anahi <luna.calderon@mi.unc.edu.ar>
2024-09-22 13:11:50 -0300

    Aprovechando el modelo para series

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,65 +1,13 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula {
-   private String nombre;
-   private int fechaDeLanzamiento;
-   private int duracionEnMinutos;
-   private boolean incluidoEnElPlan; //true/false
-   private double sumaDeLasEvaluaciones;
-   private int totalDeEvaluaciones;
+public class Pelicula extends Titulo{
+      private  String director;
 
-   public String getNombre() {
-      return nombre;
+   public String getDirector() {
+      return director;
    }
 
-   public int getFechaDeLanzamiento() {
-      return fechaDeLanzamiento;
-   }
-
-   public int getDuracionEnMinutos() {
-      return duracionEnMinutos;
-   }
-
-   public boolean isIncluidoEnElPlan() {
-      return incluidoEnElPlan;
-   }
-
-   //Setter para que el privado sirva en la otra clase porque esta en privado
-   public void setNombre(String nombre) {
-      this.nombre = nombre;
-   }
-
-   public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
-      this.fechaDeLanzamiento = fechaDeLanzamiento;
-   }
-
-   public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
-      this.incluidoEnElPlan = incluidoEnElPlan;
-   }
-
-   public void setDuracionEnMinutos(int duracionEnMinutos) {
-      this.duracionEnMinutos = duracionEnMinutos;
-   }
-
-   //creamos metodo int, queremos que nos de algo entonces ponemos get
-   //y nos sale autocompletar con la variable que queremos
-   // recordar que los metodos int necesitan return
-   public int getTotalDeEvaluaciones(){
-      return totalDeEvaluaciones;
-   }
-
-   public void muestraFichaTernica(){
-      System.out.println("El nombre de la película es: " + nombre  );
-      System.out.println("Su fecha de lanzamiento es: "+ fechaDeLanzamiento);
-      System.out.println("Duración en minutos: " + duracionEnMinutos);
-   }
-   public void evalua(double nota){
-      sumaDeLasEvaluaciones += nota;
-      totalDeEvaluaciones ++;
-   }
-   //en esta nueva nos va a dar error hasta que agreguemos return- en void no
-   public double calculaMedia(){
-
-      return sumaDeLasEvaluaciones / totalDeEvaluaciones;
+   public void setDirector(String director) {
+      this.director = director;
    }
 }
---
+++ src/com/aluracursos/screenmatch/modelos/Serie.java
@@ -0,0 +1,31 @@
+package com.aluracursos.screenmatch.modelos;
+//extends es herencia
+public class Serie extends Titulo {
+    int temporadas;
+    int episodiosPorTemporadas;
+    int minutosPorEpisodio;
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    public int getEpisodiosPorTemporadas() {
+        return episodiosPorTemporadas;
+    }
+
+    public void setEpisodiosPorTemporadas(int episodiosPorTemporadas) {
+        this.episodiosPorTemporadas = episodiosPorTemporadas;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+}
