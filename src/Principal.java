579be400df26c3621828cb01e32aa20f29941b96
anahi <luna.calderon@mi.unc.edu.ar>
2024-09-27 00:04:32 -0300

    Definiendo otras formas de construir películas y series

--- src/Principal.java
+++ src/Principal.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Pelicula miPelicula = new Pelicula("Encanto"); //crea nueva instancia -new espacio en la memory para almacenar datos dentro de ese objeto (encapsula
-        miPelicula.setFechaDeLanzamiento(2021);
+        Pelicula miPelicula = new Pelicula("Encanto", 2021); //crea nueva instancia -new espacio en la memory para almacenar datos dentro de ese objeto (encapsula
         miPelicula.setDuracionEnMinutos(120);
         System.out.println("Duración de la película: " + miPelicula.getDuracionEnMinutos());
 
@@ -21,17 +20,21 @@ public class Principal {
         System.out.println(miPelicula.getTotalDeEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
 
-        Serie casaDragon = new Serie();
-        casaDragon.setNombre("La casa del dragón");
-        casaDragon.setFechaDeLanzamiento(2022);
+        Serie casaDragon = new Serie("La casa del dragon", 2022);
         casaDragon.setTemporadas(1);
         casaDragon.setMinutosPorEpisodio(50);
         casaDragon.setEpisodiosPorTemporadas(10);
         casaDragon.muestraFichaTernica();
         System.out.println(casaDragon.getDuracionEnMinutos());
 
-        Pelicula otraPelicula = new Pelicula("Matrix");
-        otraPelicula.setFechaDeLanzamiento(1998);
+        Serie lost = new Serie("Lost", 2000);
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporadas(10);
+        lost.setMinutosPorEpisodio(50);
+        lost.muestraFichaTernica();
+        System.out.println(lost.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula("Matrix", 1998);
         otraPelicula.setDuracionEnMinutos(180);
 
         CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
@@ -52,15 +55,15 @@ public class Principal {
 
         filtroRecomendacion.filtra(episodio);
 
-        var peliculaDeBruno= new Pelicula("El señor de los anillos");
+        var peliculaDeBruno= new Pelicula("El señor de los anillos", 2001);
         peliculaDeBruno.setDuracionEnMinutos(180);
-        peliculaDeBruno.setFechaDeLanzamiento(2001);
 
         //adentro <> nos dice el tipo de array
         ArrayList<Pelicula> listaDePeliculas = new ArrayList<>();
         listaDePeliculas.add(peliculaDeBruno);
         listaDePeliculas.add(miPelicula);
         listaDePeliculas.add(otraPelicula);
+        
         System.out.println("Tamaño de la lista: " + listaDePeliculas.size());
         System.out.println("La primera película es: " + listaDePeliculas.get(0).getNombre());
         // listaDePeliculas.get(0) nos da la pelicula dentro del array
--- src/com/aluracursos/screenmatch/modelos/Serie.java
+++ src/com/aluracursos/screenmatch/modelos/Serie.java
@@ -5,6 +5,10 @@ public class Serie extends Titulo {
     int episodiosPorTemporadas;
     int minutosPorEpisodio;
 
+    public Serie(String nombre, int fechaDeLanzamiento) {
+        super(nombre, fechaDeLanzamiento);
+    }
+
     @Override
     public int getDuracionEnMinutos() {
         return temporadas * episodiosPorTemporadas * minutosPorEpisodio;
