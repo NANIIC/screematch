3ad2474c59262a01e6b746fe0a8e925a85d6a002
anahi <luna.calderon@mi.unc.edu.ar>
2024-10-02 00:05:39 -0300

    Otra estructura para representar los datos

---
+++ src/com/aluracursos/screenmatch/modelos/TituloOmdb.java
@@ -0,0 +1,4 @@
+package com.aluracursos.screenmatch.modelos;
+//String porque es laa forma en la que voy a recibir los datos
+public record TituloOmdb(String title, String year, String runtime) {
+}
